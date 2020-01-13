88cf9c657c618c3d9958229a37e0827966147e20
Sumit Pal <sumitpal547@gmail.com>
2020-01-14 00:07:26 +0530

    update script with details

--- makepdfsmaller.sh
+++ makepdfsmaller.sh
@@ -1,3 +1,9 @@
+#This script makes pdf smaller. 
+#How to run?
+#-> chmod +x makepdfsmaller.sh
+#-> ./makepdfsmaller.sh input.pdf output.pdf 100 #required resolution is 100 here. 
+
+
 fn(){
   gs -q -dNOPAUSE -dBATCH -dSAFER	-sDEVICE=pdfwrite -dPDFSETTINGS=/screen -dCompatibilityLevel=1.4 -dAutoRotatePages=/None -dEmbedAllFonts=true -dMonoImageDownsampleType=/Subsample -dMonoImageResolution=$3 -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=$3 -dColorImageDownsampleType=/Bicubic -dColorImageResolution=$3 -sOutputFile="$2" "$1"
 }
